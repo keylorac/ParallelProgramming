8a4ca1b370cf773e06c566cc74a98a73c50baa70
keylorac <keylorac@outlook.es>
2026-05-02 12:21:23 -0600

    Parte C Lab03

--- Lab03/Multiplex.c
+++ Lab03/Multiplex.c
@@ -5,10 +5,11 @@
 #include <unistd.h>
 #include <time.h>
 
-#define TOTAL_VUELOS 15
-#define PISTAS_DISPONIBLES 3
-#define K 8
+#define TOTAL_VUELOS 15 //total del vuelos en el aeropuerto
+#define PISTAS_DISPONIBLES 3 //total de pistas para aterrizar
+#define K 8 //
 #define TOTAL_PERSONAL 4 
+#define TOTAL_SERVICIOS 4
 /*Lista a realizar para el laboratorio:
     1.1 Declare sus primitivas de sincronización globales.
     1.2 Necesitará al menos un semáforo de conteo y un mutex. (LISTO)
@@ -28,10 +29,13 @@
 
 sem_t semaforo_de_pistas;
 pthread_mutex_t mutex;
+pthread_mutex_t mutex_de_barrera;
+pthread_cond_t cond_barrera;
 
 int buffer_maletas[K];
 int in = 0;
 int out = 0;
+int contador_barrera = 0; //la barrera es global para simplificar, pero idealmente seria por avion
 
 sem_t sem_espacios; //frena los aviones (no espacio)
 sem_t sem_items; //frena al personal (no items)
@@ -39,7 +43,7 @@ sem_t sem_items; //frena al personal (no items)
 pthread_mutex_t mutex_in;
 pthread_mutex_t mutex_out;
 
-int pistas_ocupadas = 0; // <---- ¡Variable compartida!
+int pistas_ocupadas = 0; //Variable compartida
 
 //¡Tomen en cuenta que printf no es thread-safe por defecto!
 void render_radar_log(int vuelo_id, const char* accion) {
@@ -72,8 +76,9 @@ void* rutina_vuelo(void* arg) {
         pthread_mutex_lock(&mutex_in); //protegemos el in
 
         buffer_maletas[in] = vuelo_id;
+        pthread_mutex_lock(&mutex);
         printf("Avion %d descargando maleta en la posicion %d \n", vuelo_id, in); 
-
+        pthread_mutex_unlock(&mutex);
         in = (in + 1) % K; //mueve circularmente
 
         pthread_mutex_unlock(&mutex_in);
@@ -82,6 +87,22 @@ void* rutina_vuelo(void* arg) {
         usleep(10000);
     }
 
+    //Barrera de los servicios de tecnicos
+
+    pthread_t servicios[TOTAL_SERVICIOS];
+    int ids_servicios[TOTAL_SERVICIOS];
+    for(int i =0; i < TOTAL_SERVICIOS; i++){
+        ids_servicios[i] = vuelo_id;
+        pthread_create(&servicios[i], NULL, servicio, &ids_servicios[i]);
+    }
+
+    //espera a todos los servicios
+    for(int i =0; i < TOTAL_SERVICIOS; i++){
+        pthread_join(servicios[i], NULL);
+    }
+    pthread_mutex_lock(&mutex);
+    printf("Vuelo %d: todos los servicios se han completado, listo para despegar\n", vuelo_id);
+    pthread_mutex_unlock(&mutex);
     //Esto simula el tiempo impredecible en pista
     int tiempo_pista = 1 + (rand() % 4); // 1 a 4 segundos
     sleep(tiempo_pista);
@@ -104,17 +125,49 @@ void* rutina_tierra(void* arg){
     while(1){
         sem_wait(&sem_items); // se ve si hay maletas
         pthread_mutex_lock(&mutex_out);
-
         int id_vuelo_maleta = buffer_maletas[out];
+        pthread_mutex_lock(&mutex);
         printf("Personal %d retira la maleta del vuelo %d de la posicion %d \n", personal_id, id_vuelo_maleta, out);
-
+        pthread_mutex_unlock(&mutex);
         out = (out + 1) % K; //movimiento circular
-
         pthread_mutex_unlock(&mutex_out);
         sem_post(&sem_espacios); //avisa que hay espacio libre
 
         usleep(10000);
     }
+}
+
+
+void barrera(){ //esta es la funcion que van a usar los "servicios tecnicos"
+    pthread_mutex_lock(&mutex_de_barrera);
+    contador_barrera++;
+
+    if(contador_barrera < TOTAL_SERVICIOS){
+        pthread_cond_wait(&cond_barrera, &mutex_de_barrera);
+    }
+    else {
+        contador_barrera = 0; //reinicia para el siguiente uso
+        pthread_cond_broadcast(&cond_barrera);
+    }
+    pthread_mutex_unlock(&mutex_de_barrera);
+}
+
+void* servicio(void* arg){
+    int vuelo_id = *(int*) arg;
+
+    int tiempo = 1 + rand()%3;
+    pthread_mutex_lock(&mutex);
+    printf("Servicio trabajando en el vuelo %d...\n", vuelo_id);
+    pthread_mutex_unlock(&mutex);
+
+    sleep(tiempo);
+
+    pthread_mutex_lock(&mutex);
+    printf("Servicio listo en el vuelo %d\n", vuelo_id);
+    pthread_mutex_unlock(&mutex);
+    barrera();
+
+    return NULL;
 }
 
 int main() {
@@ -129,6 +182,8 @@ int main() {
     pthread_mutex_init(&mutex, NULL);
     pthread_mutex_init(&mutex_in, NULL);
     pthread_mutex_init(&mutex_out, NULL);
+    pthread_mutex_init(&mutex_de_barrera, NULL);
+    pthread_cond_init(&cond_barrera, NULL);
 
     pthread_t torre_control[TOTAL_VUELOS];
     int ids_vuelos[TOTAL_VUELOS];
@@ -172,6 +227,8 @@ int main() {
     pthread_mutex_destroy(&mutex);
     pthread_mutex_destroy(&mutex_in);
     pthread_mutex_destroy(&mutex_out);
+    pthread_mutex_destroy(&mutex_de_barrera);
+    pthread_cond_destroy(&cond_barrera);
 
     return 0;
 }
